905dadaef610db4023a262242fbb1a4051178e3a
Jacob Montpetit <jacobmontpetit@Jacobs-MacBook-Pro.local>
2017-05-09 20:02:06 -0400

    updates chapter 3 problems 1 and 2

--- app/project.clj
+++ app/project.clj
@@ -1,6 +1,4 @@
-(defproject app "0.1.0-SNAPSHOT"
+(defproject my-project "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :main ^:skip-aot app.core)
--- app/src/app/core.clj
+++ app/src/app/core.clj
@@ -1,6 +1,36 @@
-(ns app.core)
+(ns app.core
+  (:gen-class))
 
-(defn foo
+(defn Ch3Pr1 
+  "problem from chapter 3 problem 1"
+  []
+  (let [string  "made a string"] 
+    (println string))
+  (let [vector (vec '(1 2 3 4 5 6) )]
+    (println (str "this is a vector " vector)))
+  (let [list '(l f 2 4 5 d :l)]
+    (println (str "this is a list " list)))
+  (let [hash-map (hash-map :k 3 :l 2 :s 15215  :j 5 :o 5 :p 5 :i 5 )]
+    (println (str "this is a hash-map " hash-map)))
+  (let [hashset (hash-set 8 :k :j 9 )]
+    (println (str "this is a hash-set " hashset)))
+  
+  )
+
+(defn Ch3Pr2
+  "problem from chapter 3 problem 2"
+  [ ]
+  (defn add100 
+    [addthis]
+    (+ addthis 100))
+  	(let [qu (quote (add100 9) )] 
+     (println qu (add100 9) )))
+
+(defn -main
   "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+  []
+  (println "Hello, World!")
+  (Ch3Pr1)
+  (Ch3Pr2)
+  )
+
